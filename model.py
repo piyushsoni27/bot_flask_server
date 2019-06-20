b359d59e3c28a70404809dc85b3ae54c7dc5dd78
piyushsoni27 <piy1995s@gmail.com>
2019-06-20 21:16:03 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 21:06:41 2019
+
+@author: piyush
+"""
+
+import tensorflow as tf 
+import numpy as np 
+
+def getTestInput(inputMessage, wList, maxLen):
+	encoderMessage = np.full((maxLen), wList.index('<pad>'), dtype='int32')
+	inputSplit = inputMessage.lower().split()
+	for index,word in enumerate(inputSplit):
+		try:
+			encoderMessage[index] = wList.index(word)
+		except ValueError:
+			continue
+	encoderMessage[index + 1] = wList.index('<EOS>')
+	encoderMessage = encoderMessage[::-1]
+	encoderMessageList=[]
+	for num in encoderMessage:
+		encoderMessageList.append([num])
+	return encoderMessageList
+
+def idsToSentence(ids, wList):
+    EOStokenIndex = wList.index('<EOS>')
+    padTokenIndex = wList.index('<pad>')
+    myStr = ""
+    listOfResponses=[]
+    for num in ids:
+        if (num[0] == EOStokenIndex or num[0] == padTokenIndex):
+            listOfResponses.append(myStr)
+            myStr = ""
+        else:
+            myStr = myStr + wList[num[0]] + " "
+    if myStr:
+        listOfResponses.append(myStr)
+    listOfResponses = [i for i in listOfResponses if i]
+    listOfResponses = list(set(listOfResponses))
+    #chosenString = ''.join(listOfResponses)
+    chosenString = listOfResponses[0]
+    #chosenString = max(listOfResponses, key=len)
+    return chosenString
