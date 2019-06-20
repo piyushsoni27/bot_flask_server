624b63c8a9fc360c51addd098b041ecc8a62af81
piyushsoni27 <piy1995s@gmail.com>
2019-06-21 01:45:13 +0530

    modified app.py

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def main():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(host="http://127.0.0.1", port="5000", debug=True)
+if __name__ == '__main__': 
+    app.run(host="localhost", port=5000, debug=True)
